d805cbd21009fa0f4d89ccecf050c688298309f9
Gabriel Coppola <gabrielcopp@live.com>
2025-03-05 15:54:25 -0300

    fix(2_add_two_numbers): fix the implementation of exercise add two numbers

--- 2_add_two_numbers/main.py
+++ 2_add_two_numbers/main.py
@@ -6,52 +6,25 @@ class ListNode:
         self.next = next
 
 class Solution:
-    # def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
-    #     dummy = ListNode()
-    #     dummy.val = None
-    #     carry = 0
-    #     while l1 or l2 or carry:
-    #         x = l1.val if l1 else 0
-    #         y = l2.val if l2 else 0
-    #         total = x + y + carry
-    #         carry = total // 10
-    #         if dummy.val is None:
-    #             dummy.val = total % 10
-    #             current = dummy
-    #         else:
-    #             current.next = ListNode(total % 10)
-    #             current = current.next
-    #         l1 = l1.next if l1 else None
-    #         l2 = l2.next if l2 else None
-    #     return dummy
-
     def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
         head = ListNode(val=None)
         current = head
         a = 0
-        while l1 or l2 is not None:
-            x = l1.val + l2.val + a
-            if (x // 10) > 0:
-                a = abs(x - 9)
-                x = 0
-            else:
-                a = 0
+        while l1 or l2 or a:
+            x = l1.val if l1 is not None else 0
+            y = l2.val if l2 is not None else 0
+            result = x + y + a
+            a = result // 10
 
             if current.val is None:
-                current.val = x
+                current.val = result % 10
             else:
-                current.next = ListNode(x)
+                current.next = ListNode(result % 10)
                 current = current.next
 
             l1 = l1.next if l1 else None
             l2 = l2.next if l2 else None
         return head
-
-
-
-
-
-
 
 def create_linked_list(arr):
     dummy = ListNode()
