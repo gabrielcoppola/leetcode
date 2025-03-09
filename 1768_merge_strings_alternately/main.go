765cfffcbdc4e3cc83bb03be8602f011326bcb54
Gabriel Coppola <gabrielcopp@live.com>
2025-03-09 18:06:15 -0300

    feat(1768_merge_strings_alternately): add merge strings alternately exercise in golang

--- 1768_merge_strings_alternately/main.go
+++ 1768_merge_strings_alternately/main.go
@@ -4,17 +4,18 @@ import "fmt"
 
 func mergeAlternately(word1 string, word2 string) string {
 	word := ""
-	for i := 0; (i < len(word1)) || (i < len(word2)); i++ {
+	len1, len2 := len(word1), len(word2)
+	for i := 0; (i < len1) || (i < len2); i++ {
 		if word1 != "" {
-			word1 = word1[0:]
 			word += word1[:1]
+			word1 = word1[1:]
 		}
 		if word2 != "" {
-			word2 = word2[0:]
 			word += word2[:1]
+			word2 = word2[1:]
 		}
 	}
-    return word
+	return word
 }
 
 func main() {
